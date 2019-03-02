173d2b4e8741bcd3ca72cb0a4a6f8cd9f1ee10e0
Thanos <545784977@qq.com>
2019-03-02 22:16:51 +0800

    resume-0.3

--- js/main.js
+++ js/main.js
@@ -1,6 +1,38 @@
 $(function() {
-	$("#fullPage").fullpage({
+
+	/* fullPage 插件配置 */
+	$('#fullPage').fullpage({
     // sectionsColor: ['#0da5d6', '#2AB561', '#DE8910', '#16BA9D', '#0DA5D6'],
     navigation: true,
+    afterLoad: function (anchor, index) {
+    	$('.section').removeClass('current')
+    	setTimeout(function () {
+    		$('.section').eq(index - 1).addClass('current')
+    	},100)
+    },
 	})
+
+	/* fullPage 屏幕小于 992 变为滑动 */
+	/*$(window).resize(function(){
+    autoScrolling();
+  });
+
+  function autoScrolling(){
+    var $ww = $(window).width();
+    if($ww < 992){
+        $.fn.fullpage.setAutoScrolling(false);
+    } else {
+        $.fn.fullpage.setAutoScrolling(true);
+    }
+  }
+
+  autoScrolling();*/
+
+
+
+
+
+
+
+
 })
